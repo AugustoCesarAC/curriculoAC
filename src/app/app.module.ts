5d80830d55b0213c3164cccc09a516b13860bd7a
Augusto Cesar <acsilvaluis@outlook.com>
2022-07-22 21:24:16 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ResumoComponent } from './resumo/resumo.component';
+import { SobreMimComponent } from './sobre-mim/sobre-mim.component';
+import { MeusProjetosComponent } from './meus-projetos/meus-projetos.component';
+import { LinguagensUsadasComponent } from './linguagens-usadas/linguagens-usadas.component';
+import { CompetenciasComponent } from './competencias/competencias.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { OrderModule } from 'ngx-order-pipe';
+import { AppRoutingModule } from './app-routing.module';
+import { ContatoComponent } from './contato/contato.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ResumoComponent,
+    SobreMimComponent,
+    MeusProjetosComponent,
+    LinguagensUsadasComponent,
+    CompetenciasComponent,
+    RodapeComponent,
+    ContatoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, OrderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/linguagens-usadas/linguagens-usadas.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-linguagens-usadas',
+  templateUrl: './linguagens-usadas.component.html',
+  styleUrls: ['./linguagens-usadas.component.css'],
+
+})
+export class LinguagensUsadasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+
+
+}
+
